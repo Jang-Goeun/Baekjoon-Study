d286c8afc347665cc65de867875b2ba21a6dd487
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2023-09-30 11:38:27 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	long long n1, n2, n3;
+
+	scanf("%ld %ld %ld", &n1, &n2, &n3);
+	printf("%ld", n1 + n2 + n3);
+
+	return 0;
+}
