3e9c1a6a5adeb08b0bc40c76c1b29ce6aabfb532
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2024-02-09 11:10:16 +0900

    [Bronze V] Title: A×B, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,22 @@
+// 장고은 2024-02-09 P10998 AXB를 출력하는 프로그램
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		int mul = A * B;
+
+		System.out. println(mul);
+	}
+
+}
